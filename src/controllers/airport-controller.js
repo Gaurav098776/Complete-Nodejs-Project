110bfc167bd637d8c970c851b1c58f635ddd0376
Gaurav singh lodhi <lodhigaurav23@gmail.com>
2025-06-10 12:01:43 +0530

    Added a airport controller

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -38,7 +38,7 @@ async function createAirport(req, res) {
 //get api = controoller
 async function getAirports(req, res){
   try{
-    const airports = await AirportService.getAirport();
+    const airports = await AirportService.getAirports();
     SuccessResponse.data = airports;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch(error){
